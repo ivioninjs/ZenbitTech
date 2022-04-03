33f25e6b1c9dfed3b739434434a2a5bb97fb8097
Mo45 <ivioninjs@gmail.com>
2022-04-04 01:12:25 +0600

    added production ip

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
+console.log(process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD : process.env.REACT_APP_DEV)
 const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD : process.env.REACT_APP_DEV
+    baseURL: "http://159.223.13.78/"
 })
 export default instance
