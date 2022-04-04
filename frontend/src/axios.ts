cef87fc30f4a93a1d1179401a9741f633f3f1116
Mo45 <ivioninjs@gmail.com>
2022-04-04 19:22:42 +0600

    added global style using styled-component

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 console.log(process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD : process.env.REACT_APP_DEV)
 const instance = axios.create({
-    baseURL: "http://159.223.13.78/"
+    baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD : process.env.REACT_APP_DEV
 })
 export default instance
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <React.Fragment>
+      <GlobalStyle />
+      <App />
+    </React.Fragment>
   </React.StrictMode>,
   document.getElementById("root")
 );
